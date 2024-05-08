2f859d3246f96e362c1311a5ca79a6d7c41e7a2b
Jan-Williams <jmpw1@uw.edu>
2024-05-08 04:00:25 -0700

    Added Lorenz and springmass hyperparam results and updated noise and train_data studies to output json dicts as opposed to plots

--- scripts/noise_results.py
+++ scripts/noise_results.py
@@ -1,8 +1,6 @@
 import RNNmpc
 import torch
 import matplotlib.pyplot as plt
-
-plt.style.use("seaborn-v0_8")
 import argparse
 import json
 
@@ -25,20 +23,23 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    "--noise_levels",
-    type=list[int],
-    help="Amount of noise to add to data.",
+    "--num_models",
+    type=int,
+    default=32,
+    help="Number of models to train for each model type.",
     required=False,
-    default=[0.0, 0.01, 0.02, 0.04, 0.06, 0.08, 0.10, 0.12],
 )
+
 
 
 args = parser.parse_args()
 best_params_dict = json.load(open(args.best_hyperparams_dict))
 data_dict = json.load(open(args.data_dict))
+num_models = args.num_models
 
-noise_levels = args.noise_levels
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+noise_levels = [0.0, 0.01, 0.02, 0.04, 0.06, 0.08, 0.10, 0.12]
+model_count = len(noise_levels) * num_models
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
 ### ESN
 alpha = best_params_dict["ESNForecaster"]["alpha"]
@@ -49,79 +50,106 @@ rho_sr = best_params_dict["ESNForecaster"]["rho_sr"]
 
 esn_list = []
 esn_dict = {}
+counter = 0
+print("Starting ESN evaluation.")
 for noise in noise_levels:
-    ret_dict, _ = RNNmpc.utils.forecast_eval(
-        "ESNForecaster",
-        data_dict=data_dict,
-        scaled=True,
-        alpha=alpha,
-        rho_sr=rho_sr,
-        beta=beta,
-        sigma_b=sigma_b,
-        sigma=sigma,
-        noise_level=noise,
-        device=device,
-    )
-
-    esn_list.append(ret_dict["fcast_dev"])
-
+    temp_list = []
+    for i in range(num_models):
+        ret_dict, _ = RNNmpc.utils.forecast_eval(
+            "ESNForecaster",
+            data_dict=data_dict,
+            scaled=True,
+            alpha=alpha,
+            rho_sr=rho_sr,
+            beta=beta,
+            sigma_b=sigma_b,
+            sigma=sigma,
+            noise_level=noise,
+            device=device,
+        )
+        counter += 1
+        if counter % 50 == 0:
+            print(str(counter) + "/" + str(num_models) + " ESN complete.")
+        temp_list.append(ret_dict["fcast_dev"])
+    esn_list.append(temp_list)
 
 adam_lr = best_params_dict["LSTMForecaster"]["adam_lr"]
 dropout_p = best_params_dict["LSTMForecaster"]["dropout_p"]
 lags = best_params_dict["LSTMForecaster"]["lags"]
 Nr = best_params_dict["LSTMForecaster"]["Nr"]
 lstm_list = []
+counter = 0
+print("Starting LSTM evaluation.")
 for noise in noise_levels:
-    ret_dict, _ = RNNmpc.utils.forecast_eval(
-        "LSTMForecaster",
-        data_dict=data_dict,
-        scaled=True,
-        adam_lr=adam_lr,
-        lags=lags,
-        hidden_dim=Nr,
-        dropout_p=dropout_p,
-        noise_level=noise,
-        device=device,
-    )
-
-    lstm_list.append(ret_dict["fcast_dev"])
+    temp_list = []
+    for i in range(num_models):
+        ret_dict, _ = RNNmpc.utils.forecast_eval(
+            "LSTMForecaster",
+            data_dict=data_dict,
+            scaled=True,
+            adam_lr=adam_lr,
+            lags=lags,
+            hidden_dim=Nr,
+            dropout_p=dropout_p,
+            noise_level=noise,
+            device=device,
+        )
+        counter += 1
+        if counter % 50 == 0:
+            print(str(counter) + "/" + str(num_models) + " LSTM complete.")
+        temp_list.append(ret_dict["fcast_dev"])
+    lstm_list.append(temp_list)
 
 adam_lr = best_params_dict["GRUForecaster"]["adam_lr"]
 dropout_p = best_params_dict["GRUForecaster"]["dropout_p"]
 lags = best_params_dict["GRUForecaster"]["lags"]
 Nr = best_params_dict["GRUForecaster"]["Nr"]
 gru_list = []
+counter = 0
+print("Starting GRU evaluation.")
 for noise in noise_levels:
-    ret_dict, _ = RNNmpc.utils.forecast_eval(
-        "GRUForecaster",
-        data_dict=data_dict,
-        scaled=True,
-        adam_lr=adam_lr,
-        lags=lags,
-        hidden_dim=Nr,
-        dropout_p=dropout_p,
-        noise_level=noise,
-        device=device,
-    )
-
-    gru_list.append(ret_dict["fcast_dev"])
+    temp_list = []
+    for i in range(num_models):
+        ret_dict, _ = RNNmpc.utils.forecast_eval(
+            "GRUForecaster",
+            data_dict=data_dict,
+            scaled=True,
+            adam_lr=adam_lr,
+            lags=lags,
+            hidden_dim=Nr,
+            dropout_p=dropout_p,
+            noise_level=noise,
+            device=device,
+        )
+        counter += 1
+        if counter % 50 == 0:
+            print(str(counter) + "/" + str(num_models) + " GRU complete.")
+        temp_list.append(ret_dict["fcast_dev"])
+    gru_list.append(temp_list)
 
 
 beta = best_params_dict["LinearForecaster"]["beta"]
 tds = best_params_dict["LinearForecaster"]["tds"]
 lin_list = []
+counter = 0
+print("Starting linear evaluation.")
 for noise in noise_levels:
-    ret_dict, _ = RNNmpc.utils.forecast_eval(
-        "LinearForecaster",
-        data_dict=data_dict,
-        scaled=True,
-        beta=beta,
-        tds=tds,
-        noise_level=noise,
-        device=device,
-    )
-
-    lin_list.append(ret_dict["fcast_dev"])
+    temp_list = []
+    for i in range(num_models):
+        ret_dict, _ = RNNmpc.utils.forecast_eval(
+            "LinearForecaster",
+            data_dict=data_dict,
+            scaled=True,
+            beta=beta,
+            tds=tds,
+            noise_level=noise,
+            device=device,
+        )
+        counter += 1
+        if counter % 50 == 0:
+            print(str(counter) + "/" + str(num_models) + " Linear complete.")
+        temp_list.append(ret_dict["fcast_dev"])
+    lin_list.append(temp_list)
 
 adam_lr = best_params_dict["FCForecaster"]["adam_lr"]
 tds = best_params_dict["FCForecaster"]["tds"]
@@ -129,30 +157,35 @@ r_width = best_params_dict["FCForecaster"]["r_width"]
 dropout_p = best_params_dict["FCForecaster"]["dropout_p"]
 
 fc_list = []
+counter = 0
+print("Starting FC evaluation.")
 for noise in noise_levels:
-    ret_dict, _ = RNNmpc.utils.forecast_eval(
-        "FCForecaster",
-        data_dict=data_dict,
-        scaled=True,
-        r_width=r_width,
-        tds=tds,
-        dropout_p=dropout_p,
-        noise_level=noise,
-        device=device,
-    )
+    temp_list = []
+    for i in range(num_models):
+        ret_dict, _ = RNNmpc.utils.forecast_eval(
+            "FCForecaster",
+            data_dict=data_dict,
+            scaled=True,
+            r_width=r_width,
+            tds=tds,
+            dropout_p=dropout_p,
+            noise_level=noise,
+            device=device,
+        )
+        counter += 1
+        if counter % 50 == 0:
+            print(str(counter) + "/" + str(num_models) + " fc complete.")
+        temp_list.append(ret_dict["fcast_dev"])
+    fc_list.append(temp_list)
 
-    fc_list.append(ret_dict["fcast_dev"])
+results_dict = {
+    "esn_list": esn_list,
+    "lstm_list": lstm_list,
+    "gru_list": gru_list,
+    "fc_list": fc_list,
+    "lin_list": lin_list,
+    "noise_levels": noise_levels
+}
 
-fig, ax = plt.subplots()
-ax.semilogy(noise_levels, lin_list, label="DMDc", linewidth=2)
-ax.semilogy(noise_levels, fc_list, label="FCN", linewidth=2)
-ax.semilogy(noise_levels, gru_list, label="GRU", linewidth=2)
-ax.semilogy(noise_levels, lstm_list, label="LSTM", linewidth=2)
-ax.semilogy(noise_levels, esn_list, label="ESN", linewidth=2)
-ax.tick_params(axis="both", which="major", labelsize=14)
-ax.tick_params(axis="both", which="minor", labelsize=14)
-ax.set_xlabel("Noise, $\sigma_{STD}$", fontsize=18)
-ax.set_ylabel("Error", fontsize=18)
-ax.set_xlim([0, max(noise_levels) * 1.35])
-ax.legend(fontsize=15, loc="center right")
-fig.savefig(args.dest + "/noise.pdf")
+with open(args.dest + "/noise_results.json", "w") as fp:
+    json.dump(results_dict, fp)
--- scripts/train_data_length.py
+++ scripts/train_data_length.py
@@ -1,7 +1,5 @@
 import RNNmpc
 import matplotlib.pyplot as plt
-
-plt.style.use("seaborn-v0_8")
 import argparse
 import json
 import torch
@@ -25,19 +23,20 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    "--train_amounts",
-    type=list[int],
-    help="Amounts of training data to consider to consider.",
-    required=False,
-    default=[500, 1000, 2000, 4000, 8000, 16000, 32000],
+    "--num_models",
+    type=int,
+    default=32,
+    help="Number of models to train for each model type.",
+    required=False
 )
 
 
 args = parser.parse_args()
 best_params_dict = json.load(open(args.best_hyperparams_dict))
 data_dict = json.load(open(args.data_dict))
+num_models = args.num_models
 
-train_amounts = args.train_amounts
+train_amounts = [500, 1000, 2000, 4000, 8000, 16000]
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -50,21 +49,29 @@ rho_sr = best_params_dict["ESNForecaster"]["rho_sr"]
 
 esn_list = []
 esn_dict = {}
+counter = 0
+model_count = len(train_amounts) * num_models
+print("Starting ESN evaluation.")
 for train_amount in train_amounts:
-    ret_dict, _ = RNNmpc.utils.forecast_eval(
-        "ESNForecaster",
-        data_dict=data_dict,
-        scaled=True,
-        alpha=alpha,
-        rho_sr=rho_sr,
-        beta=beta,
-        sigma_b=sigma_b,
-        sigma=sigma,
-        train_steps=train_amount,
-        device=device,
-    )
-
-    esn_list.append(ret_dict["fcast_dev"])
+    temp_list = []
+    for i in range(num_models):
+        ret_dict, _ = RNNmpc.utils.forecast_eval(
+            "ESNForecaster",
+            data_dict=data_dict,
+            scaled=True,
+            alpha=alpha,
+            rho_sr=rho_sr,
+            beta=beta,
+            sigma_b=sigma_b,
+            sigma=sigma,
+            train_steps=train_amount,
+            device=device,
+        )
+        counter += 1
+        if counter % 50 == 0:
+            print(str(counter) + "/" + str(num_models) + " ESN complete.")
+        temp_list.append(ret_dict["fcast_dev"])
+    esn_list.append(temp_list)
 
 
 adam_lr = best_params_dict["LSTMForecaster"]["adam_lr"]
@@ -72,56 +79,77 @@ dropout_p = best_params_dict["LSTMForecaster"]["dropout_p"]
 lags = best_params_dict["LSTMForecaster"]["lags"]
 Nr = best_params_dict["LSTMForecaster"]["Nr"]
 lstm_list = []
+counter = 0
+print("Starting LSTM evaluation.")
 for train_amount in train_amounts:
-    ret_dict, _ = RNNmpc.utils.forecast_eval(
-        "LSTMForecaster",
-        data_dict=data_dict,
-        scaled=True,
-        adam_lr=adam_lr,
-        lags=lags,
-        hidden_dim=Nr,
-        dropout_p=dropout_p,
-        train_steps=train_amount,
-        device=device,
-    )
-
-    lstm_list.append(ret_dict["fcast_dev"])
+    temp_list = []
+    for i in range(num_models):
+        ret_dict, _ = RNNmpc.utils.forecast_eval(
+            "LSTMForecaster",
+            data_dict=data_dict,
+            scaled=True,
+            adam_lr=adam_lr,
+            lags=lags,
+            hidden_dim=Nr,
+            dropout_p=dropout_p,
+            train_steps=train_amount,
+            device=device,
+        )
+        counter += 1
+        if counter % 50 == 0:
+            print(str(counter) + "/" + str(num_models) + " LSTM complete.")
+        temp_list.append(ret_dict["fcast_dev"])
+    lstm_list.append(temp_list)
 
 adam_lr = best_params_dict["GRUForecaster"]["adam_lr"]
 dropout_p = best_params_dict["GRUForecaster"]["dropout_p"]
 lags = best_params_dict["GRUForecaster"]["lags"]
 Nr = best_params_dict["GRUForecaster"]["Nr"]
 gru_list = []
+counter = 0
+print("Starting GRU evaluation.")
 for train_amount in train_amounts:
-    ret_dict, _ = RNNmpc.utils.forecast_eval(
-        "GRUForecaster",
-        data_dict=data_dict,
-        scaled=True,
-        adam_lr=adam_lr,
-        lags=lags,
-        hidden_dim=Nr,
-        dropout_p=dropout_p,
-        train_steps=train_amount,
-        device=device,
-    )
-
-    gru_list.append(ret_dict["fcast_dev"])
+    temp_list = []
+    for i in range(num_models):
+        ret_dict, _ = RNNmpc.utils.forecast_eval(
+            "GRUForecaster",
+            data_dict=data_dict,
+            scaled=True,
+            adam_lr=adam_lr,
+            lags=lags,
+            hidden_dim=Nr,
+            dropout_p=dropout_p,
+            train_steps=train_amount,
+            device=device,
+        )
+        counter += 1
+        if counter % 50 == 0:
+            print(str(counter) + "/" + str(num_models) + " GRU complete.")
+        temp_list.append(ret_dict["fcast_dev"])
+    gru_list.append(temp_list)
 
 beta = best_params_dict["LinearForecaster"]["beta"]
 tds = best_params_dict["LinearForecaster"]["tds"]
 lin_list = []
+counter = 0
+print("Starting FC evaluation.")
 for train_amount in train_amounts:
-    ret_dict, _ = RNNmpc.utils.forecast_eval(
-        "LinearForecaster",
-        data_dict=data_dict,
-        scaled=True,
-        beta=beta,
-        tds=tds,
-        train_steps=train_amount,
-        device=device,
-    )
-
-    lin_list.append(ret_dict["fcast_dev"])
+    temp_list = []
+    for i in range(num_models):
+        ret_dict, _ = RNNmpc.utils.forecast_eval(
+            "LinearForecaster",
+            data_dict=data_dict,
+            scaled=True,
+            beta=beta,
+            tds=tds,
+            train_steps=train_amount,
+            device=device,
+        )
+        counter += 1
+        if counter % 50 == 0:
+            print(str(counter) + "/" + str(num_models) + " Linear complete.")
+        temp_list.append(ret_dict["fcast_dev"])
+    lin_list.append(temp_list)
 
 adam_lr = best_params_dict["FCForecaster"]["adam_lr"]
 tds = best_params_dict["FCForecaster"]["tds"]
@@ -129,30 +157,36 @@ r_width = best_params_dict["FCForecaster"]["r_width"]
 dropout_p = best_params_dict["FCForecaster"]["dropout_p"]
 
 fc_list = []
+counter = 0
+print("Starting FC evaluation.")
 for train_amount in train_amounts:
-    ret_dict, _ = RNNmpc.utils.forecast_eval(
-        "FCForecaster",
-        data_dict=data_dict,
-        scaled=True,
-        r_width=r_width,
-        tds=tds,
-        dropout_p=dropout_p,
-        train_steps=train_amount,
-        device=device,
-    )
+    temp_list = []
+    for i in range(num_models):
+        ret_dict, _ = RNNmpc.utils.forecast_eval(
+            "FCForecaster",
+            data_dict=data_dict,
+            scaled=True,
+            r_width=r_width,
+            tds=tds,
+            dropout_p=dropout_p,
+            train_steps=train_amount,
+            device=device,
+        )
+        counter += 1
+        if counter % 50 == 0:
+            print(str(counter) + "/" + str(num_models) + " FC complete.")
+        temp_list.append(ret_dict["fcast_dev"])
+    fc_list.append(temp_list)
+    
 
-    fc_list.append(ret_dict["fcast_dev"])
+results_dict = {
+    "esn_list": esn_list,
+    "lstm_list": lstm_list,
+    "gru_list": gru_list,
+    "fc_list": fc_list,
+    "lin_list": lin_list,
+    "train_amounts": train_amounts
+}
 
-fig, ax = plt.subplots()
-ax.semilogy(train_amounts, lin_list, label="DMDc", linewidth=2)
-ax.semilogy(train_amounts, fc_list, label="FCN", linewidth=2)
-ax.semilogy(train_amounts, gru_list, label="GRU", linewidth=2)
-ax.semilogy(train_amounts, lstm_list, label="LSTM", linewidth=2)
-ax.semilogy(train_amounts, esn_list, label="ESN", linewidth=2)
-ax.tick_params(axis="both", which="major", labelsize=14)
-ax.tick_params(axis="both", which="minor", labelsize=14)
-ax.set_xlabel("Training Samples", fontsize=18)
-ax.set_ylabel("Error", fontsize=18)
-ax.set_xlim([0, max(train_amounts) * 1.35])
-ax.legend(fontsize=15, loc="center right")
-fig.savefig(args.dest + "/data_length.pdf")
+with open(args.dest + "/train_len_results.json", "w") as fp:
+    json.dump(results_dict, fp)
