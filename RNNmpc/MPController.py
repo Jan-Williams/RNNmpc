5866e5d4ed4687940c403a02d64e24e67662093c
Jan-Williams <jmpw1@uw.edu>
2024-05-20 05:54:04 -0700

    Added bounds option for MPController and used bounds in LorenzControl example

--- RNNmpc/MPController.py
+++ RNNmpc/MPController.py
@@ -1,6 +1,7 @@
 import torch
 from . import Forecasters
 
+
 class MPController:
     """LBFGS solver for minimizing MPC objective function.
 
@@ -14,7 +15,7 @@ class MPController:
         control penalty for magnitude of control input
     u_2: float
         control penalty for magnitude of control input derivative
-    
+
     Methods:
     ----------
     objective_fn(U, r_0, s_k, ref_vals, S_spin, U_spin, U_last)
@@ -42,12 +43,13 @@ class MPController:
         self.u_2 = u_2
 
     def objective_fn(
-            self,
-            U: torch.DoubleTensor,
-            ref_vals: torch.DoubleTensor,
-            s_k: torch.DoubleTensor,
-            U_last: torch.DoubleTensor,
-            **kwargs,
+        self,
+        U: torch.DoubleTensor,
+        ref_vals: torch.DoubleTensor,
+        s_k: torch.DoubleTensor,
+        U_last: torch.DoubleTensor,
+        bounds: tuple = None,
+        **kwargs,
     ) -> torch.DoubleTensor:
         """Compute objective function cost.
 
@@ -62,26 +64,30 @@ class MPController:
         U_last: torch.DoubleTensor
             last control input, dims (Nu,1)
         **kwargs
-            if ESN controller: 
+            if ESN controller:
                 specify r_k, current reservoir state, dims (Nr, 1)
             else:
                 specify U_spin, S_spin
         """
 
         if isinstance(self.forecaster, Forecasters.ESNForecaster):
-            r_k = kwargs['r_k']
+            r_k = kwargs["r_k"]
             fcast = self.forecaster.forecast(U=U, r_k=r_k, s_k=s_k)
         else:
-            U_spin = kwargs['U_spin']
-            S_spin = kwargs['S_spin']
+            U_spin = kwargs["U_spin"]
+            S_spin = kwargs["S_spin"]
             fcast = self.forecaster.forecast(U=U, U_spin=U_spin, S_spin=S_spin, s_k=s_k)
-        
+
         cost = self.dev * torch.linalg.norm(fcast - ref_vals)
         cost += self.u_1 * torch.linalg.norm(U)
         cost += self.u_2 * torch.linalg.norm(torch.diff(U))
-        cost += self.u_2 * torch.linalg.norm(U[:,0] - U_last[:, -1])
+        cost += self.u_2 * torch.linalg.norm(U[:, 0] - U_last[:, -1])
+        if bounds is not None:
+            cost += torch.linalg.norm(
+                (2 * ((U - bounds[0]) / (bounds[1] - bounds[0])) - 1) ** 8 * 5
+            )
         return cost
-    
+
     def compute_act(
         self,
         U: torch.DoubleTensor,
@@ -92,7 +98,7 @@ class MPController:
         **kwargs,
     ) -> torch.DoubleTensor:
         """Compute control action
-        
+
         Parameters:
         ----------
         U: torch.DoubleTensor
@@ -104,7 +110,7 @@ class MPController:
         U_last: torch.DoubleTensor
             last control input, dims (Nu,1)
         **kwargs
-            if ESN controller: 
+            if ESN controller:
                 specify r_k, current reservoir state, dims (Nr, 1)
             else:
                 specify U_spin, S_spin
@@ -114,19 +120,30 @@ class MPController:
             control action to minimize objective fn, dims (Nu, fcast_len)
         """
         lbfgs = torch.optim.LBFGS(
-            [U], history_size=10, max_iter=100, line_search_fn="strong_wolfe",
+            [U],
+            history_size=10,
+            max_iter=100,
+            line_search_fn="strong_wolfe",
         )
         if isinstance(self.forecaster, Forecasters.ESNForecaster):
-            r_k = kwargs['r_k']
+            r_k = kwargs["r_k"]
+
             def closure():
                 lbfgs.zero_grad()
-                objective = self.objective_fn(U=U, r_k=r_k, s_k=s_k, ref_vals=ref_vals, U_last=U_last)
+                objective = self.objective_fn(
+                    U=U,
+                    r_k=r_k,
+                    s_k=s_k,
+                    ref_vals=ref_vals,
+                    U_last=U_last,
+                    bounds=bounds,
+                )
                 objective.backward()
                 return objective
-        
+
         else:
-            U_spin = kwargs['U_spin']
-            S_spin = kwargs['S_spin']
+            U_spin = kwargs["U_spin"]
+            S_spin = kwargs["S_spin"]
 
             def closure():
                 lbfgs.zero_grad()
@@ -136,19 +153,19 @@ class MPController:
                     S_spin=S_spin,
                     U_spin=U_spin,
                     ref_vals=ref_vals,
-                    U_last = U_last,
+                    U_last=U_last,
+                    bounds=bounds,
                 )
                 objective.backward()
                 return objective
 
         lbfgs.step(closure)
-
-        if bounds is not None:
-            with torch.no_grad():
-                U = U[:,:].clamp(bounds[0], bounds[1]).clone()
+        # print(U)
+        # if bounds is not None:
+        #     with torch.no_grad():
+        #         U = U[:,:].clamp(bounds[0], bounds[1]).clone()
+        # print(U)
         return U
-
-
 
         # adam = torch.optim.Adam(
         #     [U], lr=0.001
